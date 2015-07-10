5dbaa69efcacc663487777395a5ad342f5176259
pan0007 <pan007@atlas.sk>
2015-07-10 16:57:55 +0200

    fix upload scripts

--- rel-eng/travis/upload.py
+++ rel-eng/travis/upload.py
@@ -2,9 +2,9 @@ from copr.client import CoprClient
 import sys
 
 found_project = False
-cl = CoprClient(username="nightly", login=sys.argv[1], token=sys.argv[2],
+cl = CoprClient(username="xvican01", login=sys.argv[1], token=sys.argv[2],
                 copr_url="http://copr.fedoraproject.org")
-result = cl.get_projects_list("nightly").projects_list
+result = cl.get_projects_list("xvican01").projects_list
 for project in result:
     if project.projectname == "rpg":
         found_project = True
@@ -13,5 +13,5 @@ if not found_project:
     chroot = ["fedora-21-x86_64", "fedora-21-i386", "fedora-22-x86_64",
               "fedora-22-i386", "fedora-rawhide-i386", "fedora-rawhide-x86_64"]
     cl.create_project("rpg", chroots=chroot)
-srpm_name = 'https://github.com/PavolVican/rpg/raw/srpm/' + sys.argv[3]
+srpm_name = 'https://github.com/pan0007/test/raw/srpm/' + sys.argv[3]
 cl.create_new_build("rpg", pkgs=[srpm_name])
